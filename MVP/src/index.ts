8f01427d1bfbc7c8e1b75f308d96c70f9fcde2aa
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-01 09:47:49 -0400

    configured Jest environment, created sample test

--- MVP/src/index.ts
+++ MVP/src/index.ts
@@ -1,2 +1,4 @@
 import chalk from 'chalk';
+import { pluralizeText } from './Utils/utils';
 console.log(`🌟 Everything appears to be ${chalk.greenBright('Operational')}! 🌟`);
+console.log(pluralizeText([1], 'test'));
