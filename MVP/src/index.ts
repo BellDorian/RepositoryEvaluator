55fef726a2c9d071e3eb48dde303401f33d712fa
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-01 09:49:07 -0400

    cleaning diff

--- MVP/src/index.ts
+++ MVP/src/index.ts
@@ -1,4 +1,2 @@
 import chalk from 'chalk';
-import { pluralizeText } from './Utils/utils';
 console.log(`🌟 Everything appears to be ${chalk.greenBright('Operational')}! 🌟`);
-console.log(pluralizeText([1], 'test'));
