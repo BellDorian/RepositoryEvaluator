8a14866b4ca84ab360ec919ac5dd87281540a79f
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-02 13:14:49 -0400

    added arg catcher to use for argument processing along with run file

---
+++ MVP/src/Processors/argProcessor.ts
@@ -0,0 +1,13 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+export const catchArgs = async () => {
+    const argv = await yargs(hideBin(process.argv)).argv;
+    //should check to be safe
+    if (argv._) {
+        console.log('Arguments caught..');
+        console.log(`Arguments: ${argv._.map((arg) => arg)}`);
+        const args = argv._;
+    }
+    console.log('argv obj: ', argv);
+};
--- MVP/src/index.ts
+++ MVP/src/index.ts
@@ -1,2 +1,4 @@
 import chalk from 'chalk';
+import { catchArgs } from './Processors/argProcessor';
 console.log(`🌟 Everything appears to be ${chalk.greenBright('Operational')}! 🌟`);
+catchArgs();
