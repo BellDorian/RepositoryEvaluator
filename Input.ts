0264266b72e64f0c5315584b66f9fa3f0f58720f
DorianLeona <MsDorianBell@outlook.com>
2024-09-03 09:29:27 -0400

    Guess I'm making a template Queue ... praying John doesn't find out

---
+++ Input.ts
@@ -0,0 +1,29 @@
+
+
+
+
+declare function require(name: string);
+
+const linksToModules = function ReadInput_FromFile(): Array<string>
+{
+
+const prompt = require('prompt-sync')();
+const filename = prompt("Specify filepath:\n")
+console.log('Thank you, ${filename}');
+
+let const Queue<string> 
+
+}
+
+
+/*BuildStrings
+{
+    foreach link in linksToModules{
+
+        sanitizedLink = SanitizeLink(link);
+
+
+    }
+
+
+}*/
