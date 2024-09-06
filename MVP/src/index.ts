756552480dc9ef5cfa983c3fe0cd2ca7af9703dc
DorianLeona <MsDorianBell@outlook.com>
2024-09-06 13:38:53 -0400

    Restored Processor folder. Removed ts file from outside of MVP (Input.ts)

--- MVP/src/index.ts
+++ MVP/src/index.ts
@@ -1,5 +1,7 @@
 import chalk from 'chalk';
+import { catchArgs } from './Processors/argProcessor';
 console.log(`🌟 Everything appears to be ${chalk.greenBright('Operational')}! 🌟`);
+catchArgs();
 
 import { ReadUrlFile } from './Input/Input';
 
