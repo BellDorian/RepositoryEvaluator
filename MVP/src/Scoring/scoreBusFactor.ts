44fe959bcaf1ae666e4955236aa291071ecc361a
Ben Kanter <benkanter@live.com>
2024-09-07 13:17:00 -0400

    Initial implementation for scoreCorrectness

--- MVP/src/Scoring/scoreBusFactor.ts
+++ MVP/src/Scoring/scoreBusFactor.ts
@@ -1,12 +1,11 @@
 import { Repository, NDJSONRow } from '../Types/DataTypes'
 /*
 General algorithm: 
-Pull from the repository. Get the amount of maintainers
+Pull the repository. Get the amount of maintainers
 Each maintainer from the last year with more than 10 commits to the project adds a point to the score
 Top 10% commit users give 5 points 
 
 */
 export function scoreBusFactor<T>(repo: Repository<T>): number {
-     
     return 0;
 }
