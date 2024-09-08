17ae9b6023832dadcb8e3326012194b63d8920d0
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-08 12:45:34 -0400

    cleaning diff to prevent conflicts

--- MVP/src/index.ts
+++ MVP/src/index.ts
@@ -26,10 +26,8 @@ dotenv.config();
 
 const runner = async () => {
     const repos = await buildReposFromUrls<BaseRepoQueryResponse>(mockUrls); //using mock urls for now
-    console.log(repos);
     const query = repoQueryBuilder(repos); //add an array of fields here... see Request/QueryBuilders/fields.ts for examples
     const result = await requestFromGQL<ReposFromQuery<BaseRepoQueryResponse>>(query); //result is the raw gql response... .data has your data, .errors has the errors
-    console.log(result);
     const cleanedRepos = mapGQLResultToRepos(result, repos); //mapper to clean the array of repos and add in their query results.
 
     console.log(cleanedRepos);
