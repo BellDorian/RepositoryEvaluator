a0eed97ea706cb2a4216d9f4f73f20549f69d11e
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-10 11:08:57 -0400

    checking

--- MVP/src/Output/CLI.ts
+++ MVP/src/Output/CLI.ts
@@ -1,7 +1,7 @@
 import { NDJSONRow, Repository } from '../Types/DataTypes';
 
 const convertNDJSONObjToStr = (NDJSONObj: NDJSONRow) => {
-    return `${JSON.stringify(NDJSONObj)}\n`;
+    return `${JSON.stringify(NDJSONObj)}`;
 };
 
 export const writeNDJSONToCLI = <T>(repos: Repository<T>[]) => {
