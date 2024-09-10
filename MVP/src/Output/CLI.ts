a43ad7e6584eb5148421b03a1f7ad176109143b3
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-10 11:22:02 -0400

    post test fix, trying something else for ndjson

--- MVP/src/Output/CLI.ts
+++ MVP/src/Output/CLI.ts
@@ -6,7 +6,9 @@ const convertNDJSONObjToStr = (NDJSONObj: NDJSONRow) => {
 
 export const writeNDJSONToCLI = <T>(repos: Repository<T>[]) => {
     let strBuilder = '';
+    console.log('{');
     repos.forEach((repo) => {
         console.log(convertNDJSONObjToStr(repo.NDJSONRow));
     });
+    console.log('}');
 };
