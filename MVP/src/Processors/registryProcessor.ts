e9541fed028dbc7904142f4253d6a069880971d7
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-11 22:27:09 -0400

    npm fixed

--- MVP/src/Processors/registryProcessor.ts
+++ MVP/src/Processors/registryProcessor.ts
@@ -1,3 +1,4 @@
+import { PackageURL } from '../Input/Input';
 import { fetchPackageInfo } from '../Requests/Npm/registry';
 import { transformToNDJSONRow } from '../Transform/NDJSON';
 import { QueryParams, Repository } from '../Types/DataTypes';
@@ -70,4 +71,58 @@ export const processNpmUrl = async <T>(npmUrl: string): Promise<Repository<T> | 
     } else {
         return undefined;
     }
+};
+
+/**
+ * John Leidy
+ * takes in an npm url and returns a github repository url if one was found in the registry for npm
+ * @param npmUrl npmjs.org url
+ * @returns a github repository url found from the registry
+ */
+const getRepoUrlN = async (packageName: string): Promise<string | undefined> => {
+    const packageInfo = await fetchPackageInfo(packageName);
+    if (packageInfo.repository.url) {
+        return packageInfo.repository.url;
+    }
+    return undefined;
+};
+
+/**
+ * John Leidy
+ * takes in an npm url, gets the repo from the registry, creates params, returns them if there was a repo url found, else undefined
+ * @param npmUrl npmjs.org url
+ * @returns Promise<QueryParams>|undefined
+ */
+const getOwnerRepoNameFromNPMUrlN = async (packageName: string): Promise<QueryParams | undefined> => {
+    const repoUrl = await getRepoUrlN(packageName);
+    if (repoUrl) {
+        //get the owner and name
+        const params = getOwnerNameFromGithubUrl(repoUrl);
+        //can return params or undefined
+        return params;
+    } else {
+        //repo url wasn't found
+        return undefined;
+    }
+};
+export const processNpmUrlN = async <T>(
+    npmUrlDataElement: PackageURL
+): Promise<Repository<T> | undefined> => {
+    if (npmUrlDataElement) {
+        if (npmUrlDataElement.packageName) {
+            const params = await getOwnerRepoNameFromNPMUrlN(npmUrlDataElement.packageName);
+            if (params) {
+                return {
+                    owner: params.owner,
+                    repoName: params.repoName,
+                    fileUrl: npmUrlDataElement.raw,
+                    queryResult: null,
+                    NDJSONRow: transformToNDJSONRow(npmUrlDataElement.raw),
+                };
+            } else {
+                return undefined;
+            }
+        }
+    }
+    return undefined;
 };
--- MVP/src/Processors/urlProcessor.ts
+++ MVP/src/Processors/urlProcessor.ts
@@ -1,6 +1,7 @@
+import { CleanURLSet, PackageURL, RepoURL } from '../Input/Input';
 import { Repository } from '../Types/DataTypes';
 import { processGitHubUrl } from './githubProcessor';
-import { processNpmUrl } from './registryProcessor';
+import { processNpmUrl, processNpmUrlN } from './registryProcessor';
 
 /**
  * John Leidy
@@ -23,5 +24,24 @@ export const buildReposFromUrls = async <T>(urls: string[]): Promise<Repository<
             }
         }
     }
+    return repositories;
+};
+
+export const buildReposFromUrlsN = async <T>(
+    githubUrlData: RepoURL[],
+    npmUrlData: PackageURL[]
+): Promise<Repository<T>[]> => {
+    let repositories: Repository<T>[] = [];
+    //Since the data is now split into separate types that do not have a consistent structure... process in two loops.
+    for (const npmUrlDataElement of npmUrlData) {
+        const repo = await processNpmUrlN<T>(npmUrlDataElement);
+        if (repo) {
+            repositories.push(repo);
+        }
+    }
+
+    for (const gitUrlDataElement of githubUrlData) {
+    }
+
     return repositories;
 };
