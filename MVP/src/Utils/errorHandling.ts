f1555c3ff65c4d6fd87485a592fac9406bc7aeac
DorianLeona <MsDorianBell@outlook.com>
2024-09-13 11:56:34 -0400

    Completed QA Audit; Applied changes where allowed.

--- MVP/src/Utils/errorHandling.ts
+++ MVP/src/Utils/errorHandling.ts
@@ -4,12 +4,14 @@
 // Last update :    07 September 2024 -- EST 03:12
 
 /**
- * Wraps a void function in a try-catch block, handling
- * any errors that occur during its execution. If an error
- * is caught, it logs the error message and exits with return code 1.
+ * @author Jorge Puga Hernandez
+ * @description
+ * - Wraps a void function in a try-catch block, handling
+ *   any errors that occur during its execution. If an error
+ *   is caught, it logs the error message and exits with return code 1.
  *
- * @param {() => void} func - The function to execute, which should not have any return values.
- * @param {string} message - The error message that is displayd if an error occurs.
+ * @param func - The function to execute, which should not have any return values. {@type () => void}
+ * @param message - The error message that is displayd if an error occurs. {@type string}
  *
  */
 export const ErrorWrapper = (func: () => void, message: string) => {
@@ -26,16 +28,18 @@ export const ErrorWrapper = (func: () => void, message: string) => {
 };
 
 /**
- * Wraps a function that returns a value in a try-catch block,
- * handling any errors that occur during its execution. This
- * function can accept multiple parameters. If an error is
- * caught, it logs the error message and exits with return code 1.
+ * @author Jorge Puga Hernandez
+ * @description
+ * - Wraps a function that returns a value in a try-catch block,
+ *   handling any errors that occur during its execution. This
+ *   function can accept multiple parameters.
+ * - If an error is caught, it logs the error message and exits with return code 1.
  *
  * @template T - The return type of the function being wrapped (generic).
- * @param {(...args: any[]) => T} func - The function to execute which returns T.
- * @param {string} message - The error message to display if an error occurs.
- * @param {...any[]} args - The arguments to pass to the function.
- * @returns {T} - The result of the function if no error occurs.
+ * @param func - The function to execute which returns T. {@type (...args: any[]) => T}
+ * @param message - The error message to display if an error occurs. {@type string}
+ * @param args - The arguments to pass to the function. {@type ...any[]}
+ * @returns The result of the function if no error occurs. {@type T}
  *
  */
 export const ErrorWrapperForReturns = <T>(func: (...args: any[]) => T, message: string, ...args: any[]) => {
