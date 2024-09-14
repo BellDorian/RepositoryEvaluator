8df335d9d58e113af0a9921e44ab970ad3de653e
Ben Kanter <benkanter@live.com>
2024-09-14 13:35:02 -0400

    Updated scoreCorrectness

--- MVP/src/Scoring/scoreCorrectness.ts
+++ MVP/src/Scoring/scoreCorrectness.ts
@@ -8,46 +8,16 @@
  */
 
 import { Repository, NDJSONRow } from '../Types/DataTypes'
-/*
-General algorithm: 
-Pull data using GraphQL to get number of open and closed issues. 
-Return the score as total issues divided by the number of closed issues. 
-*/
+
 export function scoreCorrectness<T>(repo: Repository<T>): number {
-    const CURRENT_DATE = new Date().toISOString(); // current date
-    const ONE_YEAR_AGO = new Date(new Date().setFullYear(new Date().getFullYear() - 1)).toISOString();
-    const ENDPOINT = "https://api.github.com/graphql";
-    const TOKEN = process.env.GITHUB_TOKEN;
-    const query = `
-    {
-        repository(owner: "${repo.owner}", name: "${repo.repoName}") {
-            openIssues: issues(states: OPEN) {
-                totalCount
-            }
-            closedIssues: issues(states: CLOSED) {
-                totalCount
-            }
-        }
-    }
-    `;
-    let openIssuesCount = 0;
-    let closedIssuesCount = 0;
-    fetch('https://api.github.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query })
-    })
-    .then((res) => res.json())
-    .then((data) => {
-    openIssuesCount = data.data.repository.openIssues.totalCount;
-    closedIssuesCount = data.data.repository.closedIssues.totalCount;
+    
+    const openIssuesCount = repo.queryResult?.openIssues?.totalCount!;
+    const closedIssuesCount = repo.queryResult?.closedIssues?.totalCount!;
+    const totalIssuesCount = openIssuesCount + closedIssuesCount;
 
     console.log(`Open issues: ${openIssuesCount}`);
     console.log(`Closed issues: ${closedIssuesCount}`);
-  })
-  .catch((error) => console.error('Error:', error));
-    return (openIssuesCount+closedIssuesCount)/closedIssuesCount;
+
+    const finalScore = 1 - (openIssuesCount/totalIssuesCount);
+    return finalScore;
 }
