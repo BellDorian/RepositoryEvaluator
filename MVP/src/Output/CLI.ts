5e0b9f616de580ed48846f5e1107530e46326da4
Bates, Anthony <ajbates@butler.edu>
2024-09-14 21:02:01 -0400

    unit testing fix

--- MVP/src/Output/CLI.ts
+++ MVP/src/Output/CLI.ts
@@ -96,14 +96,25 @@ export const processInputFile = (filePath: string) => {
  * @param NDJSONObj - The NDJSON object to convert.
  * @returns The stringified NDJSON object.
  */
+/**
+ * Converts an NDJSON object to a string.
+ * @param NDJSONObj - The NDJSON object to convert.
+ * @returns The stringified NDJSON object.
+ */
 const convertNDJSONObjToStr = (NDJSONObj: NDJSONRow) => {
     return JSON.stringify({
         URL: NDJSONObj.URL,
         NetScore: NDJSONObj.NetScore,
+        NetScore_Latency: NDJSONObj.NetScore_Latency,
         RampUp: NDJSONObj.RampUp,
+        RampUp_Latency: NDJSONObj.RampUp_Latency,
         Correctness: NDJSONObj.Correctness,
+        Correctness_Latency: NDJSONObj.Correctness_Latency,
         BusFactor: NDJSONObj.BusFactor,
+        BusFactor_Latency: NDJSONObj.BusFactor_Latency,
         ResponsiveMaintainer: NDJSONObj.ResponsiveMaintainer,
+        ResponsiveMaintainer_Latency: NDJSONObj.ResponsiveMaintainer_Latency,
         License: NDJSONObj.License,
+        License_Latency: NDJSONObj.License_Latency,
     });
 };
