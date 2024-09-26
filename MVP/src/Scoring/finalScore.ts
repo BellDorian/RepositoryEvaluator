ee7f3f0cc39d70f6e74e0b678aa807dc747e384d
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-09-26 11:14:00 -0400

    added weighted average

--- MVP/src/Scoring/finalScore.ts
+++ MVP/src/Scoring/finalScore.ts
@@ -1,23 +1,30 @@
 /**
  * @author Ben Kanter
- * 
- * 
+ *
+ *
  * @param repo - Repository to be scored
  * @param rampup - rampup time score
  * @param correctness - correctness score
  * @param busFactor - bus factor score
  * @param responsive - responsiveness score
  * @param license - license score
- * 
- * @return - final score is an average of all scores except for the license. If the license if 0, final score is 0. 
+ *
+ * @return - final score is an average of all scores except for the license. If the license if 0, final score is 0.
  */
 
-import { Repository, NDJSONRow } from '../Types/DataTypes'
+import { Repository, NDJSONRow } from '../Types/DataTypes';
 
-export function finalScore<T>(repo: Repository<T>, rampup: number, correctness: number, busFactor: number, responsive: number, license: number): number {
-    if (license == 0){
+export function finalScore<T>(
+    repo: Repository<T>,
+    rampup: number,
+    correctness: number,
+    busFactor: number,
+    responsive: number,
+    license: number
+): number {
+    if (license == 0) {
         return 0;
     }
-    const averageScore = (rampup + correctness + busFactor + responsive)/4;
+    const averageScore = (rampup * 0.1 + correctness * 0.4 + busFactor * 0.4 + responsive * 0.1) / 4;
     return averageScore;
 }
